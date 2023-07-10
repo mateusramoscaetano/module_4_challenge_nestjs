68fa50b558ea94ec182d6b23e0aa083d6d5ec700
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-10 20:14:33 -0300

    formated

--- src/accounts/accounts.service.ts
+++ src/accounts/accounts.service.ts
@@ -9,11 +9,11 @@ import { CreateAccountDto } from './dto/create-account.dto';
 import { UpdateAccountDto } from './dto/update-account.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { Account } from '@prisma/client';
-import { BankProvider } from 'src/bank/bank.provider';
-import { GetBankDto } from 'src/bank/dto/get-bank.dto';
 import { AuthService } from 'src/common/auth/auth.service';
 import { AccountProvider } from './accounts.provider';
 import { GetAccountDto } from './dto/get-account.dto';
+import { BankProvider } from 'src/bank/bank.provider';
+import { GetBankDto } from 'src/bank/dto/get-bank.dto';
 
 @Injectable()
 export class AccountsService {
@@ -72,196 +72,173 @@ export class AccountsService {
   }
 
   async findAll(): Promise<Account[]> {
-    try {
-      const bank = await this.prisma.bank.findUnique({
-        where: { id: this.bank.sub },
-      });
+    const bank = await this.prisma.bank.findUnique({
+      where: { id: this.bank.sub },
+    });
 
-      if (!bank) {
-        throw new UnauthorizedException();
-      }
+    if (!bank) {
+      throw new UnauthorizedException();
+    }
 
-      const accounts = await this.prisma.account.findMany({
-        where: { bankId: this.bank.sub },
-        select: {
-          id: true,
-          balance: true,
-          userEmail: true,
-          bankId: true,
-          user: {
-            select: {
-              name: true,
-              cpf: true,
-              birthDate: true,
-              phone: true,
-              email: true,
-            },
+    const accounts = await this.prisma.account.findMany({
+      where: { bankId: this.bank.sub },
+      select: {
+        id: true,
+        balance: true,
+        userEmail: true,
+        bankId: true,
+        user: {
+          select: {
+            name: true,
+            cpf: true,
+            birthDate: true,
+            phone: true,
+            email: true,
           },
         },
-      });
+      },
+    });
 
-      return accounts;
-    } catch (error) {
-      throw new Error(error.message);
-    }
+    return accounts;
   }
 
   async findBalance(id: number) {
-    try {
-      if (!this.account.sub || !this.bank.sub) {
-        throw new UnauthorizedException('You must be logged in');
-      }
-
-      const account = await this.prisma.account.findUnique({
-        where: { id },
-      });
-
-      if (!account) {
-        throw new BadRequestException('Account not found');
-      }
-
-      return account.balance;
-    } catch (error) {
-      throw new Error(error.message);
+    if (!this.account.sub || !this.bank.sub) {
+      throw new UnauthorizedException('You must be logged in');
     }
+
+    const account = await this.prisma.account.findUnique({
+      where: { id },
+    });
+
+    if (!account) {
+      throw new BadRequestException('Account not found');
+    }
+
+    return account.balance;
   }
 
   async findStatement(id: number) {
-    try {
-      if (!this.account.sub || !this.bank.sub) {
-        throw new UnauthorizedException('You must be logged in');
-      }
-      const account = await this.prisma.account.findUnique({
-        where: { id },
-        include: {
-          user: true,
-          Deposit: true,
-          Transfer: true,
-          Withdrawal: true,
-        },
-      });
-
-      if (!account) {
-        throw new BadRequestException('account nott found');
-      }
-
-      const deposits = account.Deposit;
-      const withdrawals = account.Withdrawal;
-      const sentTransfers = account.Transfer;
-      const receivedTransfers = await this.prisma.transfer.findMany({
-        where: { destiny_account_id: account.id },
-      });
-
-      return {
-        deposits,
-        withdrawals,
-        sentTransfers,
-        receivedTransfers,
-      };
-    } catch (error) {
-      throw new Error(error.message);
+    if (!this.account.sub || !this.bank.sub) {
+      throw new UnauthorizedException('You must be logged in');
     }
+    const account = await this.prisma.account.findUnique({
+      where: { id },
+      include: {
+        user: true,
+        Deposit: true,
+        Transfer: true,
+        Withdrawal: true,
+      },
+    });
+
+    if (!account) {
+      throw new BadRequestException('account nott found');
+    }
+
+    const deposits = account.Deposit;
+    const withdrawals = account.Withdrawal;
+    const sentTransfers = account.Transfer;
+    const receivedTransfers = await this.prisma.transfer.findMany({
+      where: { destiny_account_id: account.id },
+    });
+
+    return {
+      deposits,
+      withdrawals,
+      sentTransfers,
+      receivedTransfers,
+    };
   }
 
   async update(id: number, updateAccountDto: UpdateAccountDto) {
-    try {
-      if (!this.account.sub || !this.bank.sub) {
-        throw new UnauthorizedException('You must be logged in');
-      }
-
-      const accountToUpdate = await this.prisma.account.findUnique({
-        where: { id },
-        include: { user: true },
-      });
-
-      const bank = await this.prisma.bank.findUnique({
-        where: { id: accountToUpdate.bankId || this.bank.sub },
-      });
-
-      if (!bank) {
-        throw new UnauthorizedException();
-      }
-
-      if (updateAccountDto.user.password) {
-        const hashPassword = await this.authService.hashPassword(
-          updateAccountDto.user.password
-        );
-
-        updateAccountDto.user.password = hashPassword;
-      }
-
-      const accounts = await this.prisma.account.findMany({
-        where: { bankId: this.bank.sub },
-        include: { user: true },
-      });
-
-      if (updateAccountDto.user.cpf) {
-        const cpfExist = accounts.some(
-          (account) => account.user.cpf === updateAccountDto.user.cpf
-        );
-
-        if (
-          cpfExist &&
-          updateAccountDto.user.cpf !== accountToUpdate.user.cpf
-        ) {
-          throw new BadRequestException('CPF already exists');
-        }
-      }
-
-      if (updateAccountDto.user.email) {
-        const emailExist = accounts.some(
-          (account) => account.user.email === updateAccountDto.user.email
-        );
-
-        if (
-          emailExist &&
-          updateAccountDto.user.email !== accountToUpdate.user.email
-        ) {
-          throw new BadRequestException('Email already exists');
-        }
-      }
-
-      await this.prisma.account.update({
-        where: { id },
-        include: { user: true },
-        data: {
-          user: { update: updateAccountDto.user },
-        },
-      });
-
-      return 'Account successfully updated';
-    } catch (error) {
-      throw new Error(error.message);
+    if (!this.account.sub || !this.bank.sub) {
+      throw new UnauthorizedException('You must be logged in');
     }
+
+    const accountToUpdate = await this.prisma.account.findUnique({
+      where: { id },
+      include: { user: true },
+    });
+
+    const bank = await this.prisma.bank.findUnique({
+      where: { id: accountToUpdate.bankId || this.bank.sub },
+    });
+
+    if (!bank) {
+      throw new UnauthorizedException();
+    }
+
+    if (updateAccountDto.user.password) {
+      const hashPassword = await this.authService.hashPassword(
+        updateAccountDto.user.password
+      );
+
+      updateAccountDto.user.password = hashPassword;
+    }
+
+    const accounts = await this.prisma.account.findMany({
+      where: { bankId: this.bank.sub },
+      include: { user: true },
+    });
+
+    if (updateAccountDto.user.cpf) {
+      const cpfExist = accounts.some(
+        (account) => account.user.cpf === updateAccountDto.user.cpf
+      );
+
+      if (cpfExist && updateAccountDto.user.cpf !== accountToUpdate.user.cpf) {
+        throw new BadRequestException('CPF already exists');
+      }
+    }
+
+    if (updateAccountDto.user.email) {
+      const emailExist = accounts.some(
+        (account) => account.user.email === updateAccountDto.user.email
+      );
+
+      if (
+        emailExist &&
+        updateAccountDto.user.email !== accountToUpdate.user.email
+      ) {
+        throw new BadRequestException('Email already exists');
+      }
+    }
+
+    await this.prisma.account.update({
+      where: { id },
+      include: { user: true },
+      data: {
+        user: { update: updateAccountDto.user },
+      },
+    });
+
+    return 'Account successfully updated';
   }
 
   async remove(id: number) {
-    try {
-      if (!this.account.sub || !this.bank.sub) {
-        throw new UnauthorizedException('You must be logged in');
-      }
-
-      const accountToRemove = await this.prisma.account.findUnique({
-        where: { id },
-        include: { user: true },
-      });
-
-      if (!accountToRemove) {
-        throw new NotFoundException('Account not found');
-      }
-
-      if (accountToRemove.balance !== 0) {
-        throw new BadRequestException('Account has balance');
-      }
-
-      await this.prisma.account.delete({
-        where: { id },
-        include: { user: true },
-      });
-
-      return 'Account successfully removed';
-    } catch (error) {
-      throw new Error(error.message);
+    if (!this.account.sub || !this.bank.sub) {
+      throw new UnauthorizedException('You must be logged in');
     }
+
+    const accountToRemove = await this.prisma.account.findUnique({
+      where: { id },
+      include: { user: true },
+    });
+
+    if (!accountToRemove) {
+      throw new NotFoundException('Account not found');
+    }
+
+    if (accountToRemove.balance !== 0) {
+      throw new BadRequestException('Account has balance');
+    }
+
+    await this.prisma.account.delete({
+      where: { id },
+      include: { user: true },
+    });
+
+    return 'Account successfully removed';
   }
 }
