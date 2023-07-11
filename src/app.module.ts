910741ac1b3878e3f04cc05948cd5893fe7fc93c
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-11 12:27:20 -0300

    imports updated

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { AccountsModule } from './accounts/accounts.module';
-import { BankModule } from './bank/bank.module';
-import { TransactionsModule } from './transactions/transactions.module';
+
+import { AccountModule } from './modules/account/account.module';
+import { BankModule } from './modules/bank/bank.module';
+import { TransactionsModule } from './modules/transaction/transaction.module';
+import { PrismaModule } from './prisma';
 
 @Module({
-  imports: [PrismaModule, AccountsModule, BankModule, TransactionsModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, AccountModule, BankModule, TransactionsModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
