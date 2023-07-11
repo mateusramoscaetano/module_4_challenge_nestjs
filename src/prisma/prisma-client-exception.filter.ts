c9a62adf230786926059c8faf798339c495f0833
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-11 12:31:44 -0300

    exeception filter from prisma

---
+++ src/prisma/prisma-client-exception.filter.ts
@@ -0,0 +1,36 @@
+//src/prisma-client-exception.filter.ts
+import { ArgumentsHost, Catch, HttpStatus } from '@nestjs/common';
+import { BaseExceptionFilter } from '@nestjs/core';
+import { Prisma } from '@prisma/client';
+import { Response } from 'express';
+
+@Catch(Prisma.PrismaClientKnownRequestError)
+export class PrismaClientExceptionFilter extends BaseExceptionFilter {
+  catch(exception: Prisma.PrismaClientKnownRequestError, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    //const message = exception.message.replace(/\n/g, '');
+
+    switch (exception.code) {
+      case 'P2002':
+        const status = HttpStatus.CONFLICT;
+        response.status(status).json({
+          statusCode: status,
+          message: 'Duplicate field. Please check the provided values.',
+        });
+        break;
+      case 'P2003':
+        const requiredStatus = HttpStatus.BAD_REQUEST;
+        response.status(requiredStatus).json({
+          statusCode: requiredStatus,
+          message:
+            'Required field not provided. Please check the provided values.',
+        });
+        break;
+
+      default:
+        super.catch(exception, host);
+        break;
+    }
+  }
+}
